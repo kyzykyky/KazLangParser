de98153b63b187c9506cbb70a701b670e76249d5
Марк Цимбалюк <mark-kau@yandex.ru>
2020-03-23 19:27:14 +0600

    kazmuseum_kz

---
+++ kazmuseum_kz/Main.py
@@ -0,0 +1,12 @@
+from Methods import *
+
+
+base = 'http://www.kazmuseum.kz'
+Cat_action('ӘЛЕМ МУЗЕЙЛЕРІ', 'http://www.kazmuseum.kz/alem-mezeileri')
+Cat_action('ҚАЗАҚСТАН МУЗЕЙЛЕРІ', 'http://www.kazmuseum.kz/kazakstan-muzeileri')
+Cat_action('МУЗЕЙ ЖӘНЕ ҚОҒАМ', 'http://www.kazmuseum.kz/muzei-zhane-kogam')
+Cat_action('ЭКСПЕДИЦИЯЛАР', 'http://www.kazmuseum.kz/ekspedicialar')
+Cat_action('МУЗЕЙ ЖӘНЕ EXPO-2017', 'http://www.kazmuseum.kz/muzei-zhane-expo-2017')
+Cat_action('СҰХБАТ', 'http://www.kazmuseum.kz/s-khbat')
+print('Operation successful')
+
--- kazmuseum_kz/Methods.py
+++ kazmuseum_kz/Methods.py
@@ -1,7 +1,7 @@
 import os
-import re
 from bs4 import BeautifulSoup
 import urllib.request
+import urllib.error
 
 
 def format_sentence(sentence):
@@ -29,17 +29,18 @@ def Cat_action(Cat, Link):
 
     if not os.path.exists(Cat):
         os.mkdir(Cat)
-    pages = int(soap.findAll('a', class_='page-numbers')[-2].get_text(strip=True))
-    print(Cat + ' has ' + str(pages) + ' pages.')
-    for page in range(1, pages+1):
-        l = Link + 'page/' + str(page)
-        req = urllib.request.urlopen(l)
+    pages = soap.find('div', class_='k2Pagination')
+    page_c = int(BeautifulSoup.findAll(pages, 'li')[-1].get_text(strip=True))
+    print(Cat + ' has ' + str(page_c) + ' pages.')
+    article_c = 0
+    for page in range(1, page_c+1):
+        lnk = Link + '?start=' + str(article_c)
+        req = urllib.request.urlopen(lnk)
         html = req.read()
         soap = BeautifulSoup(html, 'html.parser')
 
-        print('\nParsing page ' + str(page) + ' ' + l)
-        materials = soap.find('div', class_='uk-grid uk-grid-collapse')
-        materials = BeautifulSoup.findAll(materials, 'a')
+        print('\nParsing page ' + str(page) + ' ' + lnk)
+        materials = soap.findAll('a', class_='moduleItemTitle')
         links = {}
         for article in materials:
             link = article.get('href')
@@ -48,27 +49,30 @@ def Cat_action(Cat, Link):
         print(links)
         for article in links:
             Article_action(Cat, article, links[article])
-
+        article_c += 10
 
 
 def Article_action(Cat, name, link):
+    link = 'http://www.kazmuseum.kz' + link
     print('Parsing ' + link + ' ...')
     fixed_name = name.replace('\"', "").replace("\'", "").replace("«", "").replace("»", "")
     adr = open(os.path.join(Cat, fixed_name) + '.url', 'w', encoding='utf-8')
     adr.write(link)
     adr.close()
-
-    req2 = urllib.request.urlopen(link)
-    html2 = req2.read()
-    soap2 = BeautifulSoup(html2, 'html.parser')
-    Text = soap2.findAll('p')
-    for line in Text:
-        line = BeautifulSoup.get_text(line)
-        f = open(os.path.join(Cat, fixed_name) + ".txt", 'a+', encoding='utf-8')
-        Line = format_sentence(line)
-        f.write(Line)
-        f.close()
-
-
-
+    try:
+        req2 = urllib.request.urlopen(link)
+        html2 = req2.read()
+        soap2 = BeautifulSoup(html2, 'html.parser')
+        Block = soap2.find('div', class_='itemView')
+        Text = Block.findAll('p')
+        for line in Text:
+            line = BeautifulSoup.get_text(line)
+            f = open(os.path.join(Cat, fixed_name) + ".txt", 'a+', encoding='utf-8')
+            line = format_sentence(line)
+            f.write(line)
+            f.close()
+    except UnicodeEncodeError:
+        print('Article ' + name + ' ' + link + ' was not parsed due to unicode encode error!')
+    except urllib.error.HTTPError:
+        pass
 
