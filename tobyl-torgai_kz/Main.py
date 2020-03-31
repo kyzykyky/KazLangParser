ba12a9f922fe551d95d95791673e78580f27c6e1
Марк Цимбалюк <mark-kau@yandex.ru>
2020-03-31 17:47:00 +0600

    tobyl-torgai_kz

---
+++ tobyl-torgai_kz/Main.py
@@ -0,0 +1,17 @@
+from Methods import *
+
+
+# Python не может читать не ascii символы в ссылках, поэтому в Lib\http\client.py
+# в методе _encode_request была произведена замена возращаемого параметра на utf-8
+Cat_action("Тарих", "http://tobyl-torgai.kz/category/тарих")
+Cat_action("Таным", "http://tobyl-torgai.kz/category/таным")
+Cat_action("Тұңғыш", "http://tobyl-torgai.kz/category/тұңғыш")
+Cat_action("Талбесік", "http://tobyl-torgai.kz/category/талбесік")
+Cat_action("Тұрмыс", "http://tobyl-torgai.kz/category/тұрмыс")
+Cat_action("Табыс", "http://tobyl-torgai.kz/category/табыс")
+Cat_action("Топжарған", "http://tobyl-torgai.kz/category/топжарған")
+Cat_action("Түрпі", "http://tobyl-torgai.kz/category/түрпі")
+Cat_action("Тасжарған", "http://tobyl-torgai.kz/category/тасжарған-2")
+Cat_action("Туынды", "http://tobyl-torgai.kz/category/туынды")
+Cat_action("Төр", "http://tobyl-torgai.kz/category/төр")
+Cat_action("Тұлға", "http://tobyl-torgai.kz/category/тұлға")
---
+++ tobyl-torgai_kz/Methods.py
@@ -0,0 +1,78 @@
+import os
+from bs4 import BeautifulSoup
+import urllib.request
+import urllib.parse
+import urllib.error
+
+
+def format_sentence(sentence):
+    sentence = sentence.split(". ")
+    lines = ''
+    for s in sentence:
+        if len(s) > 32:
+            lines += s + "\n"
+    return lines.strip()
+
+
+def replacer(name):
+    name = name.replace('?', '')
+    name = name.replace('.', '')
+    name = name.replace(';', '')
+    name = name.replace(':', '')
+    name = name.replace('\'', '').replace('\"', '').replace('»', '').replace('«', '')
+    while name.find('  ') > 0:
+        name = name.replace('  ', ' ')
+    return name
+
+
+def Cat_action(Cat, Link):
+    req = urllib.request.urlopen(Link)
+    html = req.read()
+    soap = BeautifulSoup(html, 'html.parser')
+
+    if not os.path.exists(Cat):
+        os.mkdir(Cat)
+    pages = int(soap.find('span', class_='pages').get_text(strip=True)[-1])
+    print('\n' + Cat + ' has ' + str(pages) + ' pages.')
+    for page in range(1, pages+1):
+        l = Link + '/page/' + str(page)
+        req1 = urllib.request.urlopen(l)
+        html1 = req1.read()
+        soap1 = BeautifulSoup(html1, 'html.parser')
+
+        print('\nParsing page ' + str(page) + ' ' + l)
+        materials = soap1.find('div', class_='post-listing archive-box')
+        materials = BeautifulSoup.findAll(materials, 'a')
+        links = {}
+        for article in materials:
+            link = article.get('href')
+            name = replacer(article.get_text(strip=True))
+            if name != 'Толығырақ ' and not name.isdigit() and name.find('видео') < 0 and name != '':
+                links[name] = link
+        print(links)
+        for article in links:
+            Article_action(Cat, article, links[article])
+
+
+
+def Article_action(Cat, article, link):
+    print('Parsing ' + link + ' ...')
+    adr = open(os.path.join(Cat, article) + '.url', 'w', encoding='utf-8')
+    adr.write(link)
+    adr.close()
+    try:
+        req2 = urllib.request.urlopen(link)
+        html2 = req2.read()
+        soap2 = BeautifulSoup(html2, 'html.parser')
+        Block = soap2.find('div', class_='entry')
+        Text = Block.findAll('p')
+        for line in Text:
+            line = BeautifulSoup.get_text(line)
+            f = open(os.path.join(Cat, article) + ".txt", 'a+', encoding='utf-8')
+            line = format_sentence(line)
+            f.write(line)
+            f.close()
+    except urllib.error.HTTPError:
+        pass
+
+
